c1f534cebfd01d1ace6efd213d3fd0d2b9a21ece
divanda <89817957+palsywalsyy@users.noreply.github.com>
2025-02-23 10:35:58 +0700

    cobaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ home_page = st.Page("home.py", title="Home", icon=":material/home:")
 pg = st.navigation([home_page, forecast_page])
 
 st.set_page_config(
-    page_title='Prediksi Inflasi',
+    page_title='Prediksi',
     page_icon=':money:', # This is an emoji shortcode. Could be a URL too.
 )
 pg.run()
